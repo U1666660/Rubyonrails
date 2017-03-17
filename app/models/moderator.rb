f369f9f7a2b0631b753d5e866a7ca3929461dd5c
Advanced Web <u1666660@unimail.hud.ac.uk>
2017-03-17 18:29:07 +0000

    Setup moderator validation

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -2,4 +2,9 @@ class Moderator < ApplicationRecord
   has_secure_password
 
   has_many :posts
+
+  validates :fullname, presence: true
+  validates :username, presence: true, format: {with: /@/, message: 'is not valid'}
+  validates :password, presence: true
+
 end
