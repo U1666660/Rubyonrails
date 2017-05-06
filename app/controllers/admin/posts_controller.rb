e101ec43d8038920a525c216e170814da6bbac7d
u1666660 <u16666660@unimail.hud.ac.uk>
2017-05-06 18:57:25 +0100

    Set up search function

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,11 @@
 class Admin::PostsController < Admin::ApplicationController
   def index
+    if params[:search].present?
+      @posts = Post.matching_title_or_content(params[:search]).paginate(:page => params[:page], :per_page => 10)
+    else
     @posts = Post.paginate(:page => params[:page], :per_page => 10)
-
-
-
   end
+end
 
   def new
   end
