e23bcd9c0943ee411fb20a3a305588c0bce0dfd3
u1666660 <u16666660@unimail.hud.ac.uk>
2017-05-06 23:59:41 +0100

    Visitors management

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+
 get '/login' => 'admin/sessions#new'
 get '/logout' => 'admin/sessions#destroy'
 
   namespace :admin do
     resources :posts
+    resources :visitors, only: [:index, :destroy]
     resources :tags, except: [:index]
     resources :comments, only: [:index, :update, :destroy]
     resources :sessions, only: [:new, :create, :destroy]
