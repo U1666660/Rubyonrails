34e367c91183ebd33033a64feff1830193c1632e
u1666660 <u16666660@unimail.hud.ac.uk>
2017-05-07 16:57:10 +0100

    Set list and pagination of posts on home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.where(publish: true).order(id: :desc).paginate(:page => params[:page], :per_page => (Setting.post_per_page) )
   end
 
   def show
