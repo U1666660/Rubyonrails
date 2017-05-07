dde201af5a7b6104d548da55e95d7d5155603f55
u1666660 <u16666660@unimail.hud.ac.uk>
2017-05-07 10:45:35 +0100

    Set up Messages CRUD function

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,13 +1,24 @@
 class Admin::MessagesController < Admin::ApplicationController
   def index
-    @messages = Message.all.order(id: :desc).paginate(:page => params[:page], :per_page => 10)
+     if params[:search].present?
+       @messages = Message.matching_fullname_or_content(params[:search]).paginate(:page => params[:page], :per_page => 10)
 
+    else
+    @messages = Message.all.order(id: :desc).paginate(:page => params[:page], :per_page => 10)
+end
   end
 
   def show
+    @message = Message.find(params[:id])
+    @message.mark_read
   end
 
   def update
+    @message = Message.find(params[:id])
+    @message.update(status: params[:status])
+    redirect_to :back, notice: 'Successfully updated message'
+
+
   end
 
   def destroy
