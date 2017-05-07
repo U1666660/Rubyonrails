9945156fa2c63dfc8bd6c70022d06b046d5aa2e6
u1666660 <u16666660@unimail.hud.ac.uk>
2017-05-07 17:28:50 +0100

    Set up home page posts read more and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,5 +10,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ def time_ago time
   "#{time_ago_in_words(time)} ago"
 end
 
+
+def double_digit_number n
+
+  '%02d' % n
+
+end
+
+
 end
